186f4d6160439e4469bb76e45398e6423758dee7
gulzigit22 <nabievv02@gmail.com>
2024-02-12 16:41:46 +0600

    app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pizza/modules/binchi_page/view/birinchi_view.dart';
+import 'package:pizza/modules/%20first_page/view/birinchi_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
